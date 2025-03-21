24b074ec4b0cbb7922833da6bd46f5cf372a937d
nandinikukkadapu <kukkadapunandini@gmail.com>
2025-03-21 12:05:34 +0530

    Add files via upload

---
+++ st.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title("hello world")
+st.title("this is simple stream lit app")
+
+if st.button('Say hello'):
+   st.text('Hello, Streamlit!')
+
+
+name = st.text_input('Please enter your name:')
+if name:
+   st.write(f'Hello, {name}!')
+
+
+if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
+   st.write('Thanks for uploading a file!')
